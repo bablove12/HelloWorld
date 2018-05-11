f763844a9a1570e41f1f54e91cedc41d8a126c9c
bablo <bablo@DESKTOP-9EEPDAA>
2018-05-11 16:29:34 +0900

    initial commit

---
+++ src/em1/Hello.java
@@ -0,0 +1,10 @@
+package em1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("I'm a new Java programmer");
+	}
+
+}
---
+++ src/em1/Variable.java
@@ -0,0 +1,16 @@
+package em1;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int count;
+		
+		
+		
+		count= 20;
+		
+		System.out.println(count);
+
+	}
+
+}
